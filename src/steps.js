a246f4da840c97f917e930c0037dbf4cadb84581
kaiserfaravi <shahidullahkaiserfaravi@gmail.com>
2023-02-26 22:07:08 +0600

    added first

---
+++ src/steps.js
@@ -0,0 +1,14 @@
+/*
+
+1.install react-router-dom
+2.create router using createBrowserRouter
+3.add routerProvider and pass router props
+4.Create some routes
+5.we are sending an array to createBrowserRouter,in array we need object.In object we need two things element and path.
+like--- const routers = createBrowserRouter([{
+    path:'/'
+    element:.
+    --element could be jsx or components..
+}])
+
+*/
